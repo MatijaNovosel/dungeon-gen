b5c56d07b953b250202ba469862dae6d3b34163b
Matija Novosel <mnovosel5@gmail.com>
2022-06-09 14:03:00 +0200

    Room generation wip

--- visualization/src/App.vue
+++ visualization/src/App.vue
@@ -1,25 +1,79 @@
 <template>
   <div id="center">
-    <button @click="generateRooms">Generate rooms</button>
+    <canvas :width="canvasWidth" :height="canvasHeight" ref="canvas" />
+    <button id="generate-room-btn" @click="drawRooms">Generate rooms</button>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, onMounted, ref } from "vue";
+import { Room } from "./api/client";
 import Generator from "./services/generator";
 
 export default defineComponent({
   name: "App",
   setup() {
     const generator = new Generator();
+    const canvas = ref<HTMLCanvasElement | null>(null);
+    const canvasWidth = 800;
+    const canvasHeight = 600;
 
-    const generateRooms = async () => {
-      const rooms = await generator.generateRooms(200, 800, 600, 8, 12, 8, 12);
-      console.log(rooms);
+    const getCanvasContext = () => {
+      const canvasVal = canvas.value;
+      if (canvasVal) {
+        const ctx = canvasVal.getContext("2d");
+        if (ctx) {
+          return ctx;
+        }
+      }
+      return null;
     };
 
+    const generateRooms = async (): Promise<Room[]> => {
+      const rooms = await generator.generateRooms(
+        200,
+        canvasWidth,
+        canvasHeight,
+        36,
+        42,
+        36,
+        42
+      );
+      return rooms;
+    };
+
+    const drawRooms = async () => {
+      const ctx = getCanvasContext();
+      if (ctx) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        const rooms = await generateRooms();
+        rooms.forEach((room) => {
+          ctx.fillStyle = "white";
+          ctx.fillRect(
+            room.topLeft?.x as number,
+            room.topLeft?.y as number,
+            room.width as number,
+            room.height as number
+          );
+        });
+      }
+    };
+
+    onMounted(() => {
+      const ctx = getCanvasContext();
+      if (ctx) {
+        ctx.fillStyle = "black";
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+      }
+    });
+
     return {
-      generateRooms
+      generateRooms,
+      drawRooms,
+      canvasWidth,
+      canvasHeight,
+      canvas
     };
   }
 });
@@ -30,7 +84,6 @@ export default defineComponent({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
 }
@@ -40,5 +93,17 @@ export default defineComponent({
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
+  text-align: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+}
+
+#generate-room-btn {
+  margin-top: 10px;
+}
+
+canvas {
+  border: 1px solid black;
 }
 </style>
